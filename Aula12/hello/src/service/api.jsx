82177c8a90bfa057c5368544e4bc65ac094f9dd0
mentrixmax <101875553+mateusvieiraifce@users.noreply.github.com>
2026-06-02 17:04:08 -0300

    subindo problemas

--- Aula12/hello/src/service/api.jsx
+++ Aula12/hello/src/service/api.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/v1",
+  baseURL: "https://critter-prayer-release.ngrok-free.dev/v1",
   timeout:10000,
   headers:{
     "Content-Type":"application/json",
-    Accept:'application/json'
+    Accept:'application/json',
+      "ngrok-skip-browser-warning": "1"
   }
 });
 
