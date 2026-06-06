2f3cdad66deab093ee6087fa2cb24d5a56036ff8
Jarléia Patrícia <166181184+Jarleia@users.noreply.github.com>
2026-06-06 18:23:08 -0300

    Atualização.

--- Aula12/hello/projeto_ok/src/App.jsx
+++ Aula12/hello/projeto_ok/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import DashBoard from "./DashBoard";
 import Menu from "./Menu";
+import Clientes from "./Clientes";
 import Produtos from "./Produtos";
 import User from "./User";
 import NotFound from "./NotFound";
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/Clientes" element={<Clientes />} />
         <Route path="/produtos" element={<Produtos />} />
         <Route path="/user" element={<User />} />
         <Route path="*" element={<NotFound />} />
